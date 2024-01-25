eb54c81b18fbd2345244b8c6ae9cfad411819a99
Michael Hall <michael@mbh.sh>
2024-01-25 11:30:50 +1100

    use chroder to create donor pseudo-chromosome(s)

--- workflow/scripts/plot_synteny.py
+++ workflow/scripts/plot_synteny.py
@@ -43,8 +43,9 @@ def parse_args():
         help="Path to the donor genome",
     )
     parser.add_argument(
+        "-o",
         "--outdir",
-        required=True,
+        default="./",
         help="Path to the output directory",
     )
     parser.add_argument(
@@ -90,10 +91,11 @@ def main():
 
     # align with minimap2 and then fix chromosome orientation with fixchr
     logger.info("Fix chromosome orientation...")
-    fixchr_aln = tmpdir / "fixchr.paf"
+    chroder_aln = tmpdir / "chroder.sam"
     cmd = (
-        f"minimap2 -x asm5 -t {args.threads} --eqx -c --cs {args.ref} {args.donor} | "
-        f"sort -k6,6 -k8,8n > {fixchr_aln}"
+        f"minimap2 --eqx --cs -ax asm5 -t {args.threads} {args.ref} {args.donor} > {chroder_aln}"
+        # f"minimap2 -x asm5 -t {args.threads} --eqx -c --cs {args.ref} {args.donor} | "
+        # f"sort -k6,6 -k8,8n > {fixchr_aln}"
     )
     logger.debug("Running minimap2 command: {}", cmd)
     proc = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE)
@@ -101,22 +103,26 @@ def main():
         logger.error("minimap2 failed with error:\n{}", proc.stderr.decode())
         sys.exit(1)
 
-    logger.info("Fixing chromosome orientation with fixchr...")
-    cmd = f"fixchr -r {args.ref} -q {args.donor} --dir {tmpdir} -c {fixchr_aln} -F P --contig_size 1000"
-    logger.debug("Running fixchr command: {}", cmd)
-    proc = subprocess.run(cmd, shell=True, capture_output=True)
+    logger.info("Generating psuedo-chromosome(s) for donor...")
+    prefix = tmpdir / "chroder"
+    cmd = f"chroder -F S -o {prefix} {chroder_aln} {args.ref} {args.donor}"
+    logger.debug("Running chroder command: {}", cmd)
+    proc = subprocess.run(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
     if proc.returncode != 0:
-        logger.error("fixchr failed with error:\n{}", proc.stderr.decode())
+        logger.error("chroder failed with error:\n{}", proc.stdout.decode())
         sys.exit(1)
 
-    ref = tmpdir / "fixchr.ref.filtered.fa"
-    donor = tmpdir / "fixchr.qry.filtered.fa"
+    # ref = tmpdir / f"{prefix}fixchr.ref.filtered.fa"
+    ref = args.ref
+    donor = prefix.with_suffix(".qry.fasta")
 
     logger.info("Running nucmer...")
     delta = tmpdir / "nucmer.delta"
     delta_filt = tmpdir / "nucmer.1delta"
     coords = tmpdir / "nucmer.coords"
-    cmd = f"nucmer -t {args.threads} --maxmatch --prefix={tmpdir}/nucmer --delta={delta} {ref} {donor}"
+    cmd = f"nucmer -t {args.threads} --maxmatch --prefix={tmpdir}/nucmer {ref} {donor}"
     logger.debug("Running nucmer command: {}", cmd)
     proc = subprocess.run(cmd, shell=True, capture_output=True)
     if proc.returncode != 0:
@@ -158,6 +164,10 @@ def main():
     if proc.returncode != 0:
         logger.error("syri failed with error:\n{}", proc.stderr.decode())
         sys.exit(1)
+    else:
+        syri_summary = tmpdir / "nucmer.syri.summary"
+        logger.debug("nucmer syri summary:\n{}", syri_summary.read_text())
+
     nucmer_syri = tmpdir / "nucmer.syri.out"
     if not nucmer_syri.exists():
         logger.error(f"syri failed to produce expected output {nucmer_syri}")
@@ -167,9 +177,14 @@ def main():
     cmd = f"syri --nc {args.threads} --dir {tmpdir} --prefix minimap2. -c {mm2_aln} -r {ref} -q {donor} -F P"
     logger.debug("Running syri command: {}", cmd)
     proc = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE)
+
     if proc.returncode != 0:
         logger.error("syri failed with error:\n{}", proc.stderr.decode())
         sys.exit(1)
+    else:
+        syri_summary = tmpdir / "minimap2.syri.summary"
+        logger.debug("minimap2 syri summary:\n{}", syri_summary.read_text())
+
     mm2_syri = tmpdir / "minimap2.syri.out"
     if not mm2_syri.exists():
         logger.error(f"syri failed to produce expected output {mm2_syri}")
@@ -188,6 +203,8 @@ def main():
     if proc.returncode != 0:
         logger.error("plotsr failed with error:\n{}", proc.stderr.decode())
         sys.exit(1)
+    else:
+        logger.success("Nucmer plotsr figure saved to {}", nucmer_plot)
 
     logger.info("Running plotsr on minimap2 alignment...")
     mm2_plot = outdir / "minimap2.plotsr.png"
@@ -197,6 +214,8 @@ def main():
     if proc.returncode != 0:
         logger.error("plotsr failed with error:\n{}", proc.stderr.decode())
         sys.exit(1)
+    else:
+        logger.success("Minimap2 plotsr figure saved to {}", mm2_plot)
 
     logger.success("Done!")
 
