7e8cdb154120a4b35c3f4a6c45bb00d43ae4590c
Michael Hall <michael@mbh.sh>
2024-02-01 15:19:24 +1100

    add longshot config

---
+++ workflow/scripts/callers/longshot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# send all stderr and stdout from this script to the log file
+exec &>"${snakemake_log[0]}"
+
+aln="${snakemake_input[alignment]}"
+ref="${snakemake_input[reference]}"
+finalvcf="${snakemake_output[vcf]}"
+sample="${snakemake_wildcards[sample]}"
+outvcf=$(mktemp -u).vcf
+
+longshot -AFn \
+    -b "$aln" \
+    -f "$ref" \
+    -o "$outvcf" \
+    -I 50 \
+    -s "$sample"
+
+bcftools view -o "$finalvcf" "$outvcf"
